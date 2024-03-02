fcdf18e4068577919fee652951273492dfc6dba9
kennethhoff <kenneth.98@hotmail.com>
2024-03-02 13:42:51 +0100

    try add copilot :shrug:

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -41,7 +41,7 @@ return require('packer').startup(function(use)
   })
 
   -- You can specify rocks in isolation
-  -- TODO: Re-enable this
+  -- TODO: Re-enable this (whatever it does) whenever I can get `hererocks` to install.
 --  use_rocks 'penlight'
 --  use_rocks {'lua-resty-http', 'lpeg'}
 
@@ -89,4 +89,59 @@ return require('packer').startup(function(use)
 	  'dracula/vim',
 	  as = 'dracula'
   })
+
+  -- Things I know what it does.. lol
+  use {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+	require('copilot').setup({
+  panel = {
+    enabled = true,
+    auto_refresh = false,
+    keymap = {
+      jump_prev = "[[",
+      jump_next = "]]",
+      accept = "<CR>",
+      refresh = "gr",
+      open = "<M-CR>"
+    },
+    layout = {
+      position = "bottom", -- | top | left | right
+      ratio = 0.4
+    },
+  },
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    debounce = 75,
+    keymap = {
+      accept = "<M-l>",
+      accept_word = false,
+      accept_line = false,
+      next = "<M-]>",
+      prev = "<M-[>",
+      dismiss = "<C-]>",
+    },
+  },
+  filetypes = {
+    yaml = false,
+    markdown = false,
+    help = false,
+    gitcommit = false,
+    gitrebase = false,
+    hgcommit = false,
+    svn = false,
+    cvs = false,
+    ["."] = false,
+  },
+  copilot_node_command = 'node', -- Node.js version must be > 18.x
+  server_opts_overrides = {},
+})
+    end,
+  }
+
+
+
 end)
