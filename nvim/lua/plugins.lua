e4d4a540712b05daa8e1ac017e49e6792dede2b7
kennethhoff <kenneth.98@hotmail.com>
2024-03-02 16:28:56 +0100

    Add comment plugin; Remove copilot
    I was unable to get copilot working..

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -91,56 +91,16 @@ return require('packer').startup(function(use)
   })
 
   -- Things I know what it does.. lol
-  use {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
+use {
+    'numToStr/Comment.nvim',
     config = function()
-	require('copilot').setup({
-  panel = {
-    enabled = true,
-    auto_refresh = false,
-    keymap = {
-      jump_prev = "[[",
-      jump_next = "]]",
-      accept = "<CR>",
-      refresh = "gr",
-      open = "<M-CR>"
-    },
-    layout = {
-      position = "bottom", -- | top | left | right
-      ratio = 0.4
-    },
-  },
-  suggestion = {
-    enabled = true,
-    auto_trigger = true,
-    debounce = 75,
-    keymap = {
-      accept = "<M-l>",
-      accept_word = false,
-      accept_line = false,
-      next = "<M-]>",
-      prev = "<M-[>",
-      dismiss = "<C-]>",
-    },
-  },
-  filetypes = {
-    yaml = false,
-    markdown = false,
-    help = false,
-    gitcommit = false,
-    gitrebase = false,
-    hgcommit = false,
-    svn = false,
-    cvs = false,
-    ["."] = false,
-  },
-  copilot_node_command = 'node', -- Node.js version must be > 18.x
-  server_opts_overrides = {},
-})
-    end,
-  }
+        require('Comment').setup()
+    end
+}
+
+-- TODO: Figure out how to add GitHub Copilot support.
+-- I managed to add it, but I couldn't get the keybindings to work.
+
 
 
 
