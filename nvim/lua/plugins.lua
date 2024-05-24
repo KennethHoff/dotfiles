6d97615c5489bfa649e460f8e684759ebab77691
Kenneth Hoff <kenneth.98@hotmail.com>
2024-05-24 18:05:54 +0200

    add Packer and stuff

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,101 +1,104 @@
 --- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
 return require('packer').startup(function(use)
   -- Packer can manage itself
-  use('wbthomason/packer.nvim')
+  --- Packer itself
+  use 'wbthomason/packer.nvim'
 
   -- Simple plugins can be specified as strings
-  use('rstacruz/vim-closer')
+  --- Closes brackets
+  use 'rstacruz/vim-closer'
 
   -- Lazy loading:
   -- Load on specific commands
-  use({
+  --- Creates threads; Used to running tests or whatnot from within Nvim
+  use {
 	'tpope/vim-dispatch', 
 	opt = true, 
 	cmd = {'Dispatch', 'Make', 'Focus', 'Start'}
-  })
+  }
 
   -- Load on an autocommand event
-  use({
+  --- Improves the `%` command in Vim
+  use {
   	'andymass/vim-matchup', 
 	event = 'VimEnter'
-  })
+  }
 
   -- Load on a combination of conditions: specific filetypes or commands
   -- Also run code after load (see the "config" key)
-  use( {
+  --- `Asynchronous Lint Engine`
+  use {
     'w0rp/ale',
     ft = {'sh', 'zsh', 'bash', 'c', 'cpp', 'cmake', 'html', 'markdown', 'racket', 'vim', 'tex'},
     cmd = 'ALEEnable',
     config = 'vim.cmd[[ALEEnable]]'
-  })
-
-  -- Plugins can have dependencies on other plugins
-  use({
-    'haorenW1025/completion-nvim',
-    opt = true,
-    requires = {{'hrsh7th/vim-vsnip', opt = true}, {'hrsh7th/vim-vsnip-integ', opt = true}}
-  })
-
-  -- You can specify rocks in isolation
-  -- TODO: Re-enable this (whatever it does) whenever I can get `hererocks` to install.
---  use_rocks 'penlight'
---  use_rocks {'lua-resty-http', 'lpeg'}
+  }
 
   -- Plugins can have post-install/update hooks
-  use({
+  --- Markdown previewer (Through Command)
+  use {
 	  'iamcco/markdown-preview.nvim',
 	  run = 'cd app && yarn install',
 	  cmd = 'MarkdownPreview'
-  })
+  }
 
   -- Post-install/update hook with neovim command
-  use({
+  --- Tree-sitter; Auto-updating
+  use {
 	  'nvim-treesitter/nvim-treesitter', 
 	  run = ':TSUpdate' 
-  })
-
-  -- Post-install/update hook with call of vimscript function with argument
-  use({ 
-	  'glacambre/firenvim', 
-	  run = function() vim.fn['firenvim#install'](0) 
-	  end
-  })
+  }
 
   -- Use specific branch, dependency and run lua file after load
-  use({
+  --- Status bar
+  use {
     'glepnir/galaxyline.nvim', 
     branch = 'main', 
     requires = {'kyazdani42/nvim-web-devicons', opt = true },
-  })
+  }
 
   -- Use dependency and run lua function after load
-  use({
+  --- Git integration
+  use {
     'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
     config = function() require('gitsigns').setup() end
-  })
+  }
 
-  -- You can alias plugin names
-  use({
-	  'dracula/vim',
-	  as = 'dracula'
-  })
-
-  -- Things I know what it does.. lol
+--- Adds line/block comment keybinding
 use {
     'numToStr/Comment.nvim',
     config = function()
         require('Comment').setup()
     end
 }
-  -- Mason
+--- LSP configuration
 use {
-    "williamboman/mason.nvim"
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
 }
+    use("onsails/lspkind-nvim")
+    use({
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        tag = "v<CurrentMajor>.*",
+    })
 
+    -- cmp: Autocomplete
+    use({
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        config = function()
+            require("configs.cmp")
+        end,
+    })
+
+    use("hrsh7th/cmp-nvim-lsp")
+
+    use({ "hrsh7th/cmp-path", after = "nvim-cmp" })
+
+    use({ "hrsh7th/cmp-buffer", after = "nvim-cmp" })
 
 -- TODO: Figure out how to add GitHub Copilot support.
 -- I managed to add it, but I couldn't get the keybindings to work.
