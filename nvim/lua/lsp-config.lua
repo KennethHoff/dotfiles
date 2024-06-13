d143de4ae11335454de7f1d2769244a9a5d76eff
Kenneth Hoff <kenneth.98@hotmail.com>
2024-06-13 08:36:40 +0200

    Updates

--- nvim/lua/lsp-config.lua
+++ nvim/lua/lsp-config.lua
@@ -1,2 +1,61 @@
 require("mason").setup();
 require("mason-lspconfig").setup();
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+
+local lspconfig = require("lspconfig");
+
+lspconfig.zls.setup {
+	capabilities = capabilities,
+}
+
+local luasnip = require("luasnip")
+local cmp = require("cmp")
+
+cmp.setup {
+
+  -- ... Your other configuration ...
+
+  mapping = {
+
+    -- ... Your other mappings ...
+   ['<CR>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+            if luasnip.expandable() then
+                luasnip.expand()
+            else
+                cmp.confirm({
+                    select = true,
+                })
+            end
+        else
+            fallback()
+        end
+    end),
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.locally_jumpable(1) then
+        luasnip.jump(1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    -- ... Your other mappings ...
+  },
+
+  -- ... Your other configuration ...
+}
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,105 +1,90 @@
---- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  --- Packer itself
-  use 'wbthomason/packer.nvim'
+	--- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-  -- Simple plugins can be specified as strings
-  --- Closes brackets
-  use 'rstacruz/vim-closer'
+	--- Mason and LSP Config
+	use {
+	    "williamboman/mason.nvim",
+	    "williamboman/mason-lspconfig.nvim",
+	    "neovim/nvim-lspconfig",
+	}
 
-  -- Lazy loading:
-  -- Load on specific commands
-  --- Creates threads; Used to running tests or whatnot from within Nvim
-  use {
-	'tpope/vim-dispatch', 
-	opt = true, 
-	cmd = {'Dispatch', 'Make', 'Focus', 'Start'}
-  }
+	--- Closes brackets
+	use 'rstacruz/vim-closer'
 
-  -- Load on an autocommand event
-  --- Improves the `%` command in Vim
-  use {
-  	'andymass/vim-matchup', 
-	event = 'VimEnter'
-  }
+	--- Dispatches commands; Used to running tests or whatnot from within Nvim
+	use {
+		'tpope/vim-dispatch', 
+		opt = true, 
+		cmd = {'Dispatch', 'Make', 'Focus', 'Start'}
+	}
 
-  -- Load on a combination of conditions: specific filetypes or commands
-  -- Also run code after load (see the "config" key)
-  --- `Asynchronous Lint Engine`
-  use {
-    'w0rp/ale',
-    ft = {'sh', 'zsh', 'bash', 'c', 'cpp', 'cmake', 'html', 'markdown', 'racket', 'vim', 'tex'},
-    cmd = 'ALEEnable',
-    config = 'vim.cmd[[ALEEnable]]'
-  }
+	--- Improves the `%` command in Vim
+	use {
+		'andymass/vim-matchup', 
+		event = 'VimEnter'
+	}
 
-  -- Plugins can have post-install/update hooks
-  --- Markdown previewer (Through Command)
-  use {
-	  'iamcco/markdown-preview.nvim',
-	  run = 'cd app && yarn install',
-	  cmd = 'MarkdownPreview'
-  }
+	--- `Asynchronous Lint Engine`
+	use {
+		'w0rp/ale',
+		ft = {'sh', 'zsh', 'bash', 'c', 'cpp', 'cmake', 'html', 'markdown', 'racket', 'vim', 'tex'},
+		cmd = 'ALEEnable',
+		config = 'vim.cmd[[ALEEnable]]'
+	}
 
-  -- Post-install/update hook with neovim command
-  --- Tree-sitter; Auto-updating
-  use {
-	  'nvim-treesitter/nvim-treesitter', 
-	  run = ':TSUpdate' 
-  }
+	--- Markdown previewer (Through Command)
+	use {
+		'iamcco/markdown-preview.nvim',
+		run = 'cd app && yarn install',
+		cmd = 'MarkdownPreview'
+	}
 
-  -- Use specific branch, dependency and run lua file after load
-  --- Status bar
-  use {
-    'glepnir/galaxyline.nvim', 
-    branch = 'main', 
-    requires = {'kyazdani42/nvim-web-devicons', opt = true },
-  }
+	--- Tree-sitter
+	use {
+		'nvim-treesitter/nvim-treesitter', 
+		run = ':TSUpdate' 
+	}
 
-  -- Use dependency and run lua function after load
-  --- Git integration
-  use {
-    'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
-    config = function() require('gitsigns').setup() end
-  }
+	--- Status bar
+	use {
+		'glepnir/galaxyline.nvim', 
+		branch = 'main', 
+		requires = {'kyazdani42/nvim-web-devicons', opt = true },
+	}
 
---- Adds line/block comment keybinding
-use {
-    'numToStr/Comment.nvim',
-    config = function()
-        require('Comment').setup()
-    end
-}
---- LSP configuration
-use {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-}
-    use("onsails/lspkind-nvim")
-    use({
-        "L3MON4D3/LuaSnip",
-        -- follow latest release.
-        tag = "v<CurrentMajor>.*",
-    })
+	--- Git integration
+	use {
+		'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
+		config = function() require('gitsigns').setup() end
+	}
 
-    -- cmp: Autocomplete
-    use({
-        "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
-        config = function()
-            require("configs.cmp")
-        end,
-    })
+	--- Adds line/block comment keybinding
+	use {
+		'numToStr/Comment.nvim',
+		config = function()
+		require('Comment').setup()
+		end
+	}
+	
+	--- Snippets
+	use({
+		"L3MON4D3/LuaSnip",
+		-- follow latest release.
+		tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		-- install jsregexp (optional!:).
+		run = "make install_jsregexp"
+	})
 
-    use("hrsh7th/cmp-nvim-lsp")
+	--- LSP configuration
+	use {
+		"hrsh7th/nvim-cmp",
+		requires = {
+			"hrsh7th/cmp-buffer", "hrsh7th/cmp-nvim-lsp",
+		}
+	}
 
-    use({ "hrsh7th/cmp-path", after = "nvim-cmp" })
 
-    use({ "hrsh7th/cmp-buffer", after = "nvim-cmp" })
-
--- TODO: Figure out how to add GitHub Copilot support.
--- I managed to add it, but I couldn't get the keybindings to work.
+	-- TODO: Figure out how to add GitHub Copilot support.
+	-- I managed to add it, but I couldn't get the keybindings to work.
 end)
