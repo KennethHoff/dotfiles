9935796158b08453bc3872c6e978d12b73937479
Kenneth Hoff <kenneth.98@hotmail.com>
2024-10-05 17:49:39 +0200

    yes

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -41,8 +41,6 @@ require("lazy").setup({
 })
 
 
-local lsp_zero = require('lsp-zero')
-
 -- lsp_attach is where you enable features that only work
 -- if there is a language server active in the file
 local lsp_attach = function(client, bufnr)
@@ -60,10 +58,3 @@ local lsp_attach = function(client, bufnr)
   vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
 end
 
-
-
-lsp_zero.extend_lspconfig({
-  sign_text = true,
-  lsp_attach = lsp_attach,
-  capabilities = require('cmp_nvim_lsp').default_capabilities(),
-})
--- nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,3 +1,8 @@
 return {
-	'williamboman/mason-lspconfig.nvim'
+	'williamboman/mason-lspconfig.nvim',
+	config = function() 
+		require("mason-lspconfig").setup { 
+			ensure_installed = { "lua_ls", "rust_analyzer" }
+		}
+	end
 }
