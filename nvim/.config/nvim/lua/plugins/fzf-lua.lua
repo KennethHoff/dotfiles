2252e5796b484666ad4e3a5b341d622e4c008913
Kenneth Hoff <kenneth.98@hotmail.com>
2024-10-06 19:07:50 +0200

    Add keybinds

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -3,9 +3,8 @@ return {
 				-- optional for icon support
 				dependencies = { "nvim-tree/nvim-web-devicons" },
 				config = function()
-								-- calling `setup` is optional for customization
-								require("fzf-lua").setup({})
-								vim.keymap.set("n", "<leader>ff", require('fzf-lua').files, { desc = "Fzf Files" })
-								-- nnoremap <c-P> <cmd>lua require('fzf-lua').files()<CR>
+								local fzfLua = require("fzf-lua")
+								vim.keymap.set("n", "<leader>ff", fzfLua.files, { desc = "Fzf Files" })
+								vim.keymap.set('n', '<leader>fg', '<cmd>FzfLua live_grep<CR>')
 				end
 }
