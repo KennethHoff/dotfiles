f8b51a0a970ef558dfa7586f3e50cfb8aa856164
Kenneth Hoff <kenneth.98@hotmail.com>
2024-10-06 11:15:33 +0200

    Add nvim-surround and catppuccin

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-		local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-		local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-		if vim.v.shell_error ~= 0 then
-				vim.api.nvim_echo({
-						{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-						{ out, "WarningMsg" },
-						{ "\nPress any key to exit..." },
-				}, true, {})
-				vim.fn.getchar()
-				os.exit(1)
-		end
+				local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+				local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+				if vim.v.shell_error ~= 0 then
+								vim.api.nvim_echo({
+												{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+												{ out, "WarningMsg" },
+												{ "\nPress any key to exit..." },
+								}, true, {})
+								vim.fn.getchar()
+								os.exit(1)
+				end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -29,33 +29,33 @@ vim.keymap.set('n', '<leader>e', ':NvimTreeOpen<CR>')
 
 -- Setup lazy.nvim
 require("lazy").setup({
-		spec = {
-				-- import your plugins
-				{ import = "plugins" },
-		},
-		-- Configure any other settings here. See the documentation for more details.
-		-- colorscheme that will be used when installing plugins.
-		install = { colorscheme = { "habamax" } },
-		-- automatically check for plugin updates
-		checker = { enabled = true },
+				spec = {
+								-- import your plugins
+								{ import = "plugins" },
+				},
+				-- Configure any other settings here. See the documentation for more details.
+				-- colorscheme that will be used when installing plugins.
+				install = { colorscheme = { "habamax" } },
+				-- automatically check for plugin updates
+				checker = { enabled = true },
 })
 
 
 -- lsp_attach is where you enable features that only work
 -- if there is a language server active in the file
 local lsp_attach = function(client, bufnr)
-		local opts = {buffer = bufnr}
+				local opts = {buffer = bufnr}
 
-		vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-		vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-		vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-		vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-		vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-		vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-		vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-		vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-		vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-		vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+				vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+				vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+				vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+				vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+				vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+				vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+				vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+				vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+				vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+				vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
 end
 
 
--- nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,19 +1,19 @@
 return {
-	'williamboman/mason-lspconfig.nvim',
-	config = function() 
-		require("mason").setup()
-		require("mason-lspconfig").setup {
-			ensure_installed = { "lua_ls", "rust_analyzer" }
-		}
+				"williamboman/mason-lspconfig.nvim",
+				config = function()
+								require("mason").setup()
+								require("mason-lspconfig").setup {
+												ensure_installed = { "lua_ls", "rust_analyzer" }
+								}
 
-		require("lspconfig").lua_ls.setup {
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },  -- Add any globals you want here
-					},
-				},
-			},
-		}
-	end
+								require("lspconfig").lua_ls.setup {
+												settings = {
+																Lua = {
+																				diagnostics = {
+																								globals = { "vim" },  -- Add any globals you want here
+																				},
+																},
+												},
+								}
+				end
 }
