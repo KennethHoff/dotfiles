fe9f2b860ff506df36c886e8c315f9dc8924ca6a
Kenneth Hoff <kenneth.98@hotmail.com>
2024-10-06 16:21:05 +0200

    get ts_ls working

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,3 +1,18 @@
 return {
-				"neovim/nvim-lspconfig"
+				"neovim/nvim-lspconfig",
+				config = function()
+								local lspconfig = require('lspconfig')
+
+								-- Configure ts_ls.
+								lspconfig.ts_ls.setup({
+												on_attach = function(_, bufnr)
+																-- Key mappings for LSP
+																local opts = { noremap=true, silent=true, buffer=bufnr }
+																-- Go to definition
+																vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+																-- Go to usages/references
+																vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+												end,
+								})
+				end
 }
