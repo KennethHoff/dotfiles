b2674ae48ece0a9b0be347b7f08a7e8d4a3b751b
Kenneth Hoff <kenneth.98@hotmail.com>
2024-10-07 12:28:28 +0200

    remove nvim-tree-preview

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -4,37 +4,8 @@ return {
 				lazy = false,
 				dependencies = {
 								"nvim-tree/nvim-web-devicons",
-								"b0o/nvim-tree-preview.lua",
-								"nvim-lua/plenary.nvim"
 				},
 				config = function()
-								local api = require('nvim-tree.api')
-								local preview = require('nvim-tree-preview')
-
-								require('nvim-tree').setup {
-												on_attach = function(bufnr)
-																-- Set up default keymaps
-																api.config.mappings.default_on_attach(bufnr)
-
-																local function opts(desc)
-																				return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-																end
-
-																vim.keymap.set('n', 'P', preview.watch, opts 'Preview (Watch)')
-																vim.keymap.set('n', '<Esc>', preview.unwatch, opts 'Close Preview/Unwatch')
-
-																-- Smart tab behavior: Only preview files, expand/collapse directories
-																vim.keymap.set('n', '<Tab>', function()
-																				local ok, node = pcall(api.tree.get_node_under_cursor)
-																				if ok and node then
-																								if node.type == 'directory' then
-																												api.node.open.edit()
-																								else
-																												preview.node(node, { toggle_focus = true })
-																								end
-																				end
-																end, opts 'Preview')
-												end,
-								}
-				end
+								require("nvim-tree").setup {}
+				end,
 }
