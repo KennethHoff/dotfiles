8c7ee5f59d661293472562172ae62daa3f88185f
Kenneth Hoff <kenneth.98@hotmail.com>
2024-10-17 23:32:31 +0200

    Work on dark mode

--- nixos/home/users/kennethhoff.nix
+++ nixos/home/users/kennethhoff.nix
@@ -99,10 +99,41 @@
       ];
     };
   };
-  
+
+
+  # Sets Dark Mode
+dconf.settings = {
+  "org/gnome/desktop/background" = {
+    picture-uri-dark = "file://${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.src}";
+  };
+  "org/gnome/desktop/interface" = {
+    color-scheme = "prefer-dark";
+  };
+};
+
+# gtk = {
+#   enable = true;
+#   theme = {
+#     name = "Adwaita-dark";
+#     package = pkgs.gnome.gnome-themes-extra;
+#   };
+# };
+
+# # Wayland, X, etc. support for session vars
+# systemd.user.sessionVariables = config.home-manager.users.justinas.home.sessionVariables;
+# };
+#
+# qt = {
+# enable = true;
+# platformTheme = "gnome";
+# style = "adwaita-dark";
+# };
+
+
+  ## BOILER PLATE ##
+
   # Nicely reload system units when changing configs.
   systemd.user.startServices = "sd-switch";
-  
   # Do not change this unless you know what you're doing; It's not the NixOS version, but (as of 2024-17-10) I have no idea what it is.
   home.stateVersion = "24.05";
 }
