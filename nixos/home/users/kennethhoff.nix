e8e2c95e76c49892098d908dffd6fda0d762a2a6
Kenneth Hoff <kenneth.98@hotmail.com>
2024-10-20 15:30:25 +0200

    Move hyprland to its own module

--- nixos/home/users/kennethhoff.nix
+++ nixos/home/users/kennethhoff.nix
@@ -9,6 +9,7 @@
     ../programs/neovim.nix
     ../programs/tmux.nix
     ../programs/waybar.nix
+    ../programs/hyprland.nix
   ];
 
   home.username = "kennethhoff";
@@ -29,7 +30,6 @@
     ];
   };
 
-
   programs.zsh = {
     enable = true;
     oh-my-zsh = {
@@ -44,74 +44,6 @@
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
-  };
-  programs.kitty = {
-    enable = true;
-    settings = {
-      font_family = "JetBrains Mono";
-      bold_font = "auto";
-      italic_font = "auto";
-      bold_italic_font = "auto";
-    };
-  };
-  programs.rofi.enable = true;
-
-  wayland.windowManager.hyprland = {
-    enable = true;
-    settings = {
-      "$mainMod" = "SUPER";
-      general = {
-        no_focus_fallback = true;
-        gaps_in = 2;
-        gaps_out = 5;
-        border_size = 5;
-      };
-      animations = {
-        enabled = true;
-        animation = [
-          "windows, 1, 7, default"
-          "windowsOut, 1, 7, default"
-          "border, 1, 10, default"
-          "borderangle, 1, 8, default"
-          "fade, 1, 7, default"
-          "workspaces, 1, 3, default"
-        ];
-      };
-      bind = [
-        "$mainMod, Q, exec, kitty"
-        "$mainMod, P, exit"
-        "$mainMod, C, killactive"
-        "$mainMod, R, exec, rofi -show drun"
-        "$mainMod, M, fullscreen, 1"
-        "$mainMod, H, movefocus, l"
-        "$mainMod, J, movefocus, d"
-        "$mainMod, K, movefocus, u"
-        "$mainMod, L, movefocus, r"
-        "$mainMod, S, exec, grim -g \"$(slurp -o -r -c '##ff0000ff')\" -t ppm - | satty --filename - --fullscreen --output-filename ~/Pictures/Screenshots/satty-$(date '+%Y%m%d-%H:%M:%S').png"
-
-        "$mainMod, 1, workspace, 1"
-        "$mainMod, 2, workspace, 2"
-        "$mainMod, 3, workspace, 3"
-        "$mainMod, 4, workspace, 4"
-        "$mainMod, 5, workspace, 5"
-        "$mainMod, 6, workspace, 6"
-        "$mainMod, 7, workspace, 7"
-        "$mainMod, 8, workspace, 8"
-        "$mainMod, 9, workspace, 9"
-        "$mainMod, 0, workspace, 10"
-
-        "$mainMod SHIFT, 1, movetoworkspace, 1"
-        "$mainMod SHIFT, 2, movetoworkspace, 2"
-        "$mainMod SHIFT, 3, movetoworkspace, 3"
-        "$mainMod SHIFT, 4, movetoworkspace, 4"
-        "$mainMod SHIFT, 5, movetoworkspace, 5"
-        "$mainMod SHIFT, 6, movetoworkspace, 6"
-        "$mainMod SHIFT, 7, movetoworkspace, 7"
-        "$mainMod SHIFT, 8, movetoworkspace, 8"
-        "$mainMod SHIFT, 9, movetoworkspace, 9"
-        "$mainMod SHIFT, 0, movetoworkspace, 10"
-      ];
-    };
   };
 
   # xdg = {
