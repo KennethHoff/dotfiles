5e2c3cbb99f86c5f0d199e84d8749ec7bbe2611f
Kenneth Hoff <kenneth.98@hotmail.com>
2024-10-20 23:41:43 +0200

    Add Steam

--- nixos/hosts/nixos_dell/configuration.nix
+++ nixos/hosts/nixos_dell/configuration.nix
@@ -85,6 +85,15 @@
     zsh.enable = true;
     hyprland.enable = true;
     waybar.enable = true;
+
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+      gamescopeSession.enable = true;
+      extraCompatPackages = with pkgs; [proton-ge-bin];
+    };
   };
 
   security.rtkit.enable = true;
