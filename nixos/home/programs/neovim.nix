8a0919517a06df012baf92e02cb73845395df635
Kenneth Hoff <kenneth.98@hotmail.com>
2024-10-24 21:58:39 +0200

    [nixos/programs:neovim] Add `gcc` to list of required packages

--- nixos/home/programs/neovim.nix
+++ nixos/home/programs/neovim.nix
@@ -8,17 +8,18 @@
     ../../utils.nix
   ];
   home.packages = with pkgs; [
+    neovim
+    gcc # Needed for <some reason>. Quick googling says it's needed by `Treesitter`.
     ripgrep
     fd
     lua-language-server
     nil # Unofficial Nix Language Server
     nixfmt-rfc-style # Official Nix Code Formatter
     stylua
-    neovim
   ];
 
   # Symlink the `~/.config/nvim` to the `~/dotfiles/nvim/.config/nvim` directory
   # This essentially mirrors `GNU stow` in home-manager
-  # This is an anti-pattern in Nix land, but I've gotta be pragmatic.. I still use non-NixOS OSes after all..
+  # This is an anti-pattern in Nix land, but I've gotta be pragmatic.. I still use non-Nix OSes after all..
   xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink "${config.dotfiles}/nvim/.config/nvim/";
 }
